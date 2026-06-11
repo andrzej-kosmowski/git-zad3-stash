607701c9907ce9333186af2bdd481334dfa4840c
Andrzej <bazylek11@gmail.com>
2026-06-11 12:18:34 +0200

    feat: lastName added

--- UserProfile.java
+++ UserProfile.java
@@ -4,14 +4,17 @@ public class UserProfile {
     private String name;
     private String email;
     private int age;
+    private String lastName;
 
     public UserProfile(String name, String email, int age) {
         this.name = name;
         this.email = email;
         this.age = age;
+        this.lastName = lastName;
     }
 
     public String getName() { return name; }
     public String getEmail() { return email; }
     public int getAge() { return age; }
+    public String getLastName() { return lastName; }
 }
